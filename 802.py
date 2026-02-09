02da4bab92ae30e53f0ce579c3453f6bbb31ef7e
Raghav Mishra <raghav051006@gmail.com>
2026-02-09 20:24:59 +0530

    commit 1

---
+++ 802.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def eventualSafeNodes(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: List[int]
+        """
+        n = len(graph)
+        safe = {}
+
+        def dfs(i):
+            if i in safe:
+                return safe[i]
+            safe[i] = False
+            for nei in graph[i]:
+                if not dfs(nei):
+                    return False
+            safe[i] = True
+            return True
+
+        res = []
+        for i in range(n):
+            if dfs(i):
+                res.append(i)
+        return res
